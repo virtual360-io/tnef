51a789d3e7b20d42378814706e14ca89aca22696
Victor Campos <victorcampos@Victors-MacBook-Pro.local>
2023-02-03 14:27:59 -0300

    fix: avoid cd

--- lib/tnef.rb
+++ lib/tnef.rb
@@ -8,7 +8,7 @@ module Tnef
 
   def self.unpack(winmail_io, &block)
     in_tmp_dir do |dir|
-      IO.popen("#{Tnef::Executable.path} --number-backups --save-body --body-pref th -K", "wb") do |tnef|
+      IO.popen("#{Tnef::Executable.path} --number-backups --save-body --body-pref th -K --directory=#{dir}", "wb") do |tnef|
         tnef.write(winmail_io.read)
         tnef.close
       end
@@ -20,11 +20,6 @@ module Tnef
   end
 
   def self.in_tmp_dir(&block)
-    Dir.mktmpdir do |dir|
-      FileUtils.cd(dir) do
-        yield(dir)
-      end
-    end
+    yield(Dir.mktmpdir)
   end
-
 end
